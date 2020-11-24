3ef079da1899db7fe46dba0636075a7ec3baecd2
KanekiSenapi <v.kaneki.senpai@gmail.com>
2020-11-24 02:05:32 +0100

    fix sys env

--- src/main/java/pl/aogiri/vsm/channelcreator/dao/SecretsDAO.java
+++ src/main/java/pl/aogiri/vsm/channelcreator/dao/SecretsDAO.java
@@ -5,7 +5,10 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class SecretsDAO {
+    @Value("${SERVER_HOST}")
     public static String HOST = System.getenv("SERVER_HOST");
+    @Value("${SERVER_LOGIN}")
     public static String LOGIN = System.getenv("SERVER_LOGIN");
+    @Value("${SERVER_PASSWORD}")
     public static String PASSWORD = System.getenv("SERVER_PASSWORD");
 }
